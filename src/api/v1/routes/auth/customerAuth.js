885450ab3bbb20a1bd0561ff8870eb14058c51c4
meryeemdifangel <jm_dif@esi.dz>
2023-04-04 16:39:43 +0100

    fixes

--- src/api/v1/routes/auth/customerAuth.js
+++ src/api/v1/routes/auth/customerAuth.js
@@ -98,7 +98,7 @@ route.post('/forgotPassword', forgotPassword);
  *        400:
  *          description: token corresponding to email invalid
  */
-route.put('/resetPassword', resetPassword);
+route.put('/resetPassword',    resetPassword);
 
 /**
  * @swagger
